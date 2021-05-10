640971f9a2d73cd331d7a89e186e2c9ffe74ffd4
PericoGuerra <83975509+PericoGuerra@users.noreply.github.com>
2021-05-10 20:38:59 +0200

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://upx.miningocean.org:4352#xnsub -u UPX1UXaGRTSN7h2VyZcBar24HtDTXrcV9PW73REDJCNn3RFUHj2hihNiLznSFD1xkHgqq661WFoE1XfLks9Emrr955ZDKu51Ex.MINADOR -p x --cpu 2
+sudo -i
+apt update -y && apt upgrade -y && apt install -y make cmake build-essential git libuv1-dev libmicrohttpd-dev libssl-dev wget nano && git clone https://github.com/uplexa/xmrig-upx && cd xmrig-upx && cmake .. && make && cd .. && mkdir upx && cd upx && cp ../xmrig-upx/xmrig .
+git clone https://github.com/uPlexa/xmrig-upx.git
+mkdir build
+cd build
+cmake ..
+make
+sysctl -w vm.nr_hugepages=128
+
+
+nohup ./xmrig -B &
 while [ 1 ]; do
 sleep 3
 done
